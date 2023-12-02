c979150307ceed23ae434c4b47ee9c5108a3c268
Tuấn Huỳnh <tuanhq@nichietsuvn.com>
2023-12-02 23:59:45 +0700

    add authen

--- chat.py
+++ chat.py
@@ -1,7 +1,6 @@
-import time
+import bcrypt
 import streamlit as st
 from openai import OpenAI
-
 
 st.set_page_config(
    page_title="Template AI",
@@ -12,55 +11,66 @@ st.set_page_config(
 
 st.title("ChatGPT 3.5")
 
-# Sidebar
-with st.sidebar:
-    add_title = st.title("Temple AI",)
-    add_logo = st.image('logo.png', width=180)
+is_authenticated = False
+st.sidebar.title("Login")
+password = st.sidebar.text_input("Password",type='password')
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+if st.sidebar.button("Login", type='secondary'):
+    # hashed = bcrypt.hashpw("my=password".encode('utf-8'), bcrypt.gensalt())
+    if bcrypt.checkpw(password.encode('utf-8'), b'$2b$12$TNba.gndNgsMxHeDg97dfuwUuHLLFYzkCiCd0nibJjPJ4tLSsiVqC'):
+        is_authenticated = True
+        # st.sidebar.selectbox("Select your model",("ChatGPT 3.5", "ChatGPT 4.0"))
+    else:
+        st.sidebar.error("Invalid password")
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+if is_authenticated:
+    st.write("You have successfully logged in")
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-if "messages" not in  st.session_state:
-    st.session_state.messages = []
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if "messages" not in  st.session_state:
+        st.session_state.messages = []
 
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("Hãy nhập vào yêu cầu?"):
-    st.session_state.messages.append(
-        {
-            "role": "user",
-            "content": prompt
-        }
-    )
+    if prompt := st.chat_input("Enter your question..."):
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": prompt
+            }
+        )
 
-    with st.chat_message('user'):
-        st.markdown(prompt)
+        with st.chat_message('user'):
+            st.markdown(prompt)
 
-    with st.chat_message('assistant'):
-        full_res = ""
-        holder = st.empty()
+        with st.chat_message('assistant'):
+            full_res = ""
+            holder = st.empty()
 
-        for response in client.chat.completions.create(
-            model = st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_res += (response.choices[0].delta.content or "")
-            holder.markdown(full_res + "▌")
+            for response in client.chat.completions.create(
+                model = st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            ):
+                full_res += (response.choices[0].delta.content or "")
+                holder.markdown(full_res + "▌")
+                holder.markdown(full_res)
             holder.markdown(full_res)
-        holder.markdown(full_res)
 
-    st.session_state.messages.append(
-        {
-            "role": "assistant",
-            "content": full_res
-        }
-    )
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": full_res
+            }
+        )
+else:
+    st.error("You are not logged in")
+    
